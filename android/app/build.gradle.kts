2681e8aa51b89561e53e33f784b05f0fe8e61ac7
Ajai-Joseph <ajaijoseph363@gmail.com>
2025-11-12 11:21:48 +0530

    migrated to latest flutter

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
+    id("com.android.application")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id "dev.flutter.flutter-gradle-plugin"
-    id "org.jetbrains.kotlin.android"
-    id "com.google.gms.google-services"
+    id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -13,12 +13,12 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
@@ -26,7 +26,7 @@ android {
         applicationId = "com.example.aj_chat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -36,7 +36,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.debug
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
@@ -46,5 +46,7 @@ flutter {
 }
 
 dependencies {
+    implementation "androidx.work:work-runtime-ktx:2.7.1"
     implementation platform('com.google.firebase:firebase-bom:32.5.0')
+    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:1.2.2'
 }
--- android/app/src/main/kotlin/com/example/aj_chat/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/aj_chat/MainActivity.kt
@@ -1,50 +1,5 @@
 package com.example.aj_chat
 
-import android.app.PictureInPictureParams
-import android.os.Build
-import android.util.Rational
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
-    private var isUserInCall = false // Flag to track if the user is on VideoCallScreen
-
-    override fun onCreate(savedInstanceState: android.os.Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Create the MethodChannel for communication with Flutter
-        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, "com.example.app/pip").setMethodCallHandler { call, result ->
-            when (call.method) {
-                "setUserInCall" -> {
-                    // Update the isUserInCall flag
-                    isUserInCall = call.argument<Boolean>("isInCall") ?: false
-                    result.success(null)
-                }
-                "enterPipMode" -> {
-                    if (isUserInCall && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        enterPictureInPictureModeWithAspectRatio()
-                    }
-                    result.success(null)
-                }
-                else -> result.notImplemented()
-            }
-        }
-    }
-
-    private fun enterPictureInPictureModeWithAspectRatio() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val aspectRatio = Rational(16, 9) // Aspect ratio for PiP (16:9 for video calls)
-            val pipBuilder = PictureInPictureParams.Builder()
-            pipBuilder.setAspectRatio(aspectRatio)
-            enterPictureInPictureMode(pipBuilder.build())
-        }
-    }
-
-//    override fun onUserLeaveHint() {
-//        super.onUserLeaveHint()
-//        // Trigger PiP when the user minimizes the app only if they are in VideoCallScreen
-//        if (isUserInCall && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            enterPictureInPictureModeWithAspectRatio()
-//        }
-//    }
-}
+class MainActivity : FlutterActivity()
